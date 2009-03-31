89e41f945a4449f5ffe0e00a4e1ec42d07a64bd5
Christian Schilling <christian@initcrash.net>
2009-03-31 15:42:31 +0200

    added exaile track display plugin

---
+++ ledmatrix_exaile.py
@@ -0,0 +1,34 @@
+
+import dbus
+import xl.plugins as plugins
+
+PLUGIN_NAME = "LedMatrix Plugin"
+PLUGIN_AUTHORS = ['Some Dude <somedude@someplace.com>', ]
+PLUGIN_VERSION = '0.1'
+PLUGIN_DESCRIPTION = r"""Displays information via ledmatrixd"""
+PLUGIN_ENABLED = False
+PLUGIN_ICON = None
+
+
+led_matrix = False
+CONNS = plugins.SignalContainer()
+
+
+def initialize():
+    global led_matrix
+    conn = dbus.SessionBus()
+    obj = conn.get_object("net.initcrash.LedMatrix","/net/initcrash/LedMatrix")
+    led_matrix = dbus.Interface(obj,dbus_interface="net.initcrash.LedMatrix")
+    led_matrix.Init("192.168.2.10")
+    led_matrix.ClearScreen()
+    led_matrix.PrintStr("Exaile")
+    led_matrix.ScrollLeft(30)
+    CONNS.connect(APP.player, 'play-track', play_track)
+    return True
+
+def destroy():
+    CONNS.disconnect_all()
+
+def play_track(exaile, track):
+    led_matrix.ClearScreen()
+    led_matrix.PrintStr("\a%s \rby %s \bon %s" % (track.title, track.artist, track.album))
